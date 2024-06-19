50805ce38b22685f693a02f1b7465e9bfdf995b1
Khanh Duy <duykhanh471@protonmail.com>
2024-06-19 16:03:35 +0700

    Another update, kinda finished it
    Gotta work on the `update()` and the `sort()` function soon

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,48 @@
 use std::fs::{self, OpenOptions};
 use std::io::Write;
 
-pub struct Todo {
-    name: String,
-    project: String
+pub fn add(task: &str) {
+    let mut file = OpenOptions::new().write(true).append(true).create(true).open("todo.txt").expect("Something went wrong went creating `todo.txt` before adding the give task.");
+    if let Err(e) = writeln!(file, "{}", task) {
+        eprintln!("This error is expected when adding any tasks to the todo.txt, here is the error: {}", e);
+    }        
+}
+    
+pub fn list() {
+    cat("todo.txt");
 }
 
-impl Todo {
-    pub fn new(name: String, project: String) -> Self{
-        Todo { name: name, project: project }
-    }
-    pub fn add(task: &str) {
-        let mut file = OpenOptions::new().write(true).create(true).open("todo.txt").expect("Something went wrong went creating `todo.txt` before adding the give task.");
-        if let Err(e) = writeln!(file, "{}", task) {
-            eprintln!("This error is expected when adding any tasks to the todo.txt, here is the error: {}", e);
-        } 
-    }
-    
-    pub fn list() {
-        cat("todo.txt");
-    }
-    
-    
+pub fn update(id: &str) {
+    println!("The function is being implemented. The {} is not updated yet", id);
+} 
+
+pub fn delete(id: usize) {
+    let content = fs::read_to_string("todo.txt").expect("Can't open the file");
+    let updated = content.lines().enumerate().filter_map(|(n, s)| (n != id)
+        .then_some(s.to_string() + "\n")).collect::<String>(); 
+    let mut file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open("todo.txt")
+        .expect("Something went wrong when creating `todo.txt` before adding the given task.");
+    file.write_all(updated.as_bytes()).expect("This is expected");
 }
-
-
+    
+pub fn clear() {
+    fs::remove_file("todo.txt").expect("Can't clear the todo.txt");
+}
 // Preview file's content.
 fn cat(filename: &str) {
-    println!("{}", fs::read_to_string(filename).expect("Unable to list all the tasks. \n If you haven't added any tasks, please add any before proceeding"))
+    fs::read_to_string(filename).expect("Unable to list all the tasks. \n If you haven't added any tasks, please add any before proceeding").lines().enumerate().for_each(|(num, line)| println!("{} {}", num, line));
 }
 
 pub fn help() {
     println!(r#"
+You should write a task like this
+`[YOUR TASK] +[PROJECT'S or GROUP'S NAME]` 
+To follow the `todo.txt` convention. And you can quickly sort the task list based on the GROUP'S name.
+
 Usage "todotxt-rs [OPTION] [TASK]"
     - add: Add [TASK] to the `todo.txt` file
     - list: List all tasks.
--- src/main.rs
+++ src/main.rs
@@ -4,29 +4,34 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    
     if args.len() == 1 {
         todotxt_rs::help();
         std::process::exit(1);
     }
-
+    
     let command = &args[1];
     let suffix = &args[2..].iter().cloned().collect::<Vec<_>>().join(" ");
     
     match command.as_str() {
         "add" => {
-            todotxt_rs::Todo::add(suffix);
+            todotxt_rs::add(suffix);
         },
         "list" => {
-            todotxt_rs::Todo::list();
+            todotxt_rs::list();
         },
         "update" => {
-            
+            todotxt_rs::update(suffix);
         },
-        "del" => {
-            
-        }
+        "delete" => {
+            let id = suffix.trim().parse().unwrap();
+            todotxt_rs::delete(id);
+        },
         "help" => {
             todotxt_rs::help();
+        },
+        "clear" => {
+            todotxt_rs::clear();
         }
         _ => {
             todotxt_rs::help();
